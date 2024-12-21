d84f4ea362886c9d0fde0749a076ad53e5f424bd
armnoob <18750880958@163.com>
2024-12-21 08:22:37 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    extends: ['expo', 'prettier',"plugin:react-hooks/recommended",'plugin:react-native/all',],
+    plugins: ["simple-import-sort",'react-native',"prettier","react-hooks"],
+    rules: {
+      "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    'react-native/no-color-literals': 'off',
+    "react-native/no-inline-styles": 0,
+      'prettier/prettier': 'error',
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": [
+        "warn", {
+          "additionalHooks": "(useRecoilCallback|useRecoilTransaction_UNSTABLE)"
+        }
+      ],
+    },
+    overrides: [
+      {
+        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+        rules: {
+          'simple-import-sort/imports': [
+            'error',
+            {
+              groups: [
+                // Packages `react` related packages come first.
+                [
+                  '^react',
+                  'recoil',
+                ],
+                // Internal packages.
+                [
+                  '@components/*',
+                  '@hooks/*',
+                  '@views/*',
+                  '@store/*',
+                  '@utils/*',
+                  '@type/*'
+                ],
+                // Side effect imports.
+                ['^\\u0000'],
+                ['@assets/*'],
+                // Parent imports. Put `..` last.
+                ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                // Other relative imports. Put same-folder imports and `.` last.
+                ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                // Style imports.
+                ['^.+\\.?(css)$', '^.+\\.?(json)$']
+              ]
+            }
+          ],
+          'react-native/no-raw-text': [
+            'error',
+            {skip: ['ThemedText']}
+          ],
+        }
+      }
+    ]
+  };  
