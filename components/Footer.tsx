a6ec2cdbc1f29431af6d26e5a63469958565def5
xingzai <18750880958@163.com>
2025-01-03 14:58:03 +0800

    调整图片上下移动动画

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { useWindowDimensions, View } from 'react-native';
+import { Image, useWindowDimensions, View } from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedStyle,
@@ -9,11 +9,10 @@ import Animated, {
 import { Center } from './ui/center';
 
 import { HStack } from '@/components/ui/hstack';
-import { Image } from '@/components/ui/image';
 import { Text } from '@/components/ui/text';
 
 export default function Footer({ scrollRef }: any) {
-  const { width: sw } = useWindowDimensions();
+  const { width: sw, height: sh } = useWindowDimensions();
   const layout = useRef<any>();
   const scrollOffset = useScrollViewOffset(scrollRef);
   const handleLayout = (event: any) => {
@@ -28,8 +27,8 @@ export default function Footer({ scrollRef }: any) {
         {
           translateY: interpolate(
             scrollOffset.value,
-            [layout.current?.y || 0, (layout.current?.y || 0) + (468 / 1920) * sw],
-            [(-(468 / 1920) * sw) / 5, ((468 / 1920) * sw) / 5],
+            [(layout.current?.y || 0) - sh / 2, (layout.current?.y || 0) - sh / 4],
+            [(-(468 / 1920) * sw) / 5, 0],
           ),
         },
       ],
@@ -41,8 +40,7 @@ export default function Footer({ scrollRef }: any) {
         style={{
           width: '100%',
           height: (468 / 1920) * sw,
-          overflow: 'hidden',
-          backgroundColor: '#000',
+          backgroundColor: '#fff',
         }}>
         <Animated.View
           style={[
@@ -50,9 +48,9 @@ export default function Footer({ scrollRef }: any) {
             headerAnimatedStyle,
           ]}>
           <Image
+            style={{ position: 'absolute', zIndex: -10, width: '100%', height: '100%' }}
             alt=""
             source={require('@/assets/images/house.png')}
-            className="absolute -z-10 h-full w-full"
             resizeMode="cover"
           />
           <Center>
@@ -61,30 +59,6 @@ export default function Footer({ scrollRef }: any) {
           </Center>
         </Animated.View>
       </View>
-      {/*  <Center className="w-full" style={{ height: (468 / 1920) * sw }}>
-        <Image
-          alt=""
-          source={require('@/assets/images/house.png')}
-          className="absolute -z-10 h-full w-full"
-          resizeMode="cover"
-        />
-        <Center>
-          <Text className="text-[50px] font-[600] text-[#fff]">欢迎联系我们</Text>
-          <Text className="mt-[30px] w-[659px] text-center text-[20px] font-[400] leading-7 text-[#fff]">{`Realm-Estate 领域地产目前在加州洛杉矶地区开展业务。\n致力于帮助您完成项目开发升级，也寻找豪宅项目开发投资机会。`}</Text>
-        </Center>
-      </Center> */}
-      {/*  <Animated.View style={[{ height: (468 / 1920) * sw,width:'100%',justifyContent:'center',alignItems:'center' },headerAnimatedStyle]}>
-        <Image
-          alt=""
-          source={require('@/assets/images/house.png')}
-          className="absolute -z-10 h-full w-full"
-          resizeMode="cover"
-        />
-        <Center>
-          <Text className="text-[50px] font-[600] text-[#fff]">欢迎联系我们</Text>
-          <Text className="mt-[30px] w-[659px] text-center text-[20px] font-[400] leading-7 text-[#fff]">{`Realm-Estate 领域地产目前在加州洛杉矶地区开展业务。\n致力于帮助您完成项目开发升级，也寻找豪宅项目开发投资机会。`}</Text>
-        </Center>
-      </Animated.View> */}
       <HStack className="items-center bg-black" style={{ height: (286 / 1920) * sw }}>
         <HStack
           style={{
@@ -93,9 +67,9 @@ export default function Footer({ scrollRef }: any) {
             height: (40 / 1920) * sw,
           }}>
           <Image
+            style={{ width: '100%', height: '100%' }}
             alt=""
             source={require('@/assets/images/real.png')}
-            className="h-full w-full"
             resizeMode="cover"
           />
         </HStack>
@@ -111,33 +85,33 @@ export default function Footer({ scrollRef }: any) {
         </Text>
         <HStack style={{ marginLeft: (89 / 1920) * sw }} className="items-center">
           <Image
+            style={{ width: 21, height: 21 }}
             alt=""
             source={require('@/assets/images/phone.png')}
-            className="h-[21px] w-[21px]"
             resizeMode="cover"
           />
           <Image
+            style={{ marginLeft: 50, height: 32, width: 32 }}
             alt=""
             source={require('@/assets/images/email.png')}
-            className="ml-[50px] h-[32px] w-[32px]"
             resizeMode="cover"
           />
           <Image
+            style={{ marginLeft: 50, height: 26, width: 12 }}
             alt=""
             source={require('@/assets/images/facebook.png')}
-            className="ml-[50px] h-[26px] w-[12px]"
             resizeMode="cover"
           />
           <Image
+            style={{ marginLeft: 50, height: 27, width: 26 }}
             alt=""
             source={require('@/assets/images/video.png')}
-            className="ml-[50px] h-[27px] w-[26px]"
             resizeMode="cover"
           />
           <Image
             alt=""
+            style={{ marginLeft: 50, height: 25, width: 25 }}
             source={require('@/assets/images/titok.png')}
-            className="ml-[50px] h-[25px] w-[25px]"
             resizeMode="cover"
           />
           <Text className="absolute -bottom-[32px] right-0 text-[18px] font-[400] text-[#fff]/[0.5]">
